8c39e722748253ff10257a91d58f8d619a71910b
Hannahvic <hannahvl1987@hotmail.com>
2020-05-25 22:37:06 +0100

    initial files

---
+++ script.js
@@ -0,0 +1,56 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  burger.addEventListener('click', () => {
+    // Toggle Nav
+    nav.classList.toggle('nav-active');
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+    // Burger Animation
+    burger.classList.toggle('toggle');
+  });
+};
+
+const app = () => {
+  navSlide();
+};
+
+app();
+
+const text = document.querySelector('.quote');
+const quote = text.textContent;
+const splitText = quote.split('');
+text.textContent = '';
+
+for (let i = 0; i < splitText.length; i++) {
+  text.innerHTML += '<span>' + splitText[i] + '</span>';
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++;
+  if (char === splitText.length) {
+    complete();
+    return;
+  }
+}
+
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
